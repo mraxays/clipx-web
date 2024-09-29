9c1e4ad326931a6864f3dbba8f18e7354a4f22ba
MrAkshayAS <akshau72p@gmail.com>
2024-09-29 15:11:56 +0530

    Site Map Update

--- routes/index.js
+++ routes/index.js
@@ -8,21 +8,54 @@ router.get('/', function(req, res, next) {
 router.get('/sitemap.xml', function(req, res, next) {
   res.header('Content-Type', 'application/xml');
 
-  const lastModifiedDate = '2024-09-25';
+  const lastModifiedDate = '2024-09-29'; // Update this as necessary
 
   const sitemapXml = `
     <?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      <!-- Home Page -->
       <url>
         <loc>${req.protocol}://${req.get('host')}/</loc>
         <lastmod>${lastModifiedDate}</lastmod>
         <changefreq>monthly</changefreq>
         <priority>1.0</priority>
       </url>
+
+      <!-- Section: Why ClipX -->
+      <url>
+        <loc>${req.protocol}://${req.get('host')}/#why</loc>
+        <lastmod>${lastModifiedDate}</lastmod>
+        <changefreq>monthly</changefreq>
+        <priority>0.8</priority>
+      </url>
+
+      <!-- Section: Documentation -->
+      <url>
+        <loc>${req.protocol}://${req.get('host')}/#document</loc>
+        <lastmod>${lastModifiedDate}</lastmod>
+        <changefreq>monthly</changefreq>
+        <priority>0.8</priority>
+      </url>
+
+      <!-- Section: Uses -->
+      <url>
+        <loc>${req.protocol}://${req.get('host')}/#uses</loc>
+        <lastmod>${lastModifiedDate}</lastmod>
+        <changefreq>monthly</changefreq>
+        <priority>0.8</priority>
+      </url>
+
+      <!-- Section: Installation -->
+      <url>
+        <loc>${req.protocol}://${req.get('host')}/#installation</loc>
+        <lastmod>${lastModifiedDate}</lastmod>
+        <changefreq>monthly</changefreq>
+        <priority>0.8</priority>
+      </url>
     </urlset>
   `;
 
-  res.send(sitemapXml.trim()); 
+  res.send(sitemapXml.trim());
 });
 
 
